63aed6ec64c1e24457b9e80596599c5f76167805
Brent Kerby <blkerby@gmail.com>
2024-08-03 09:35:16 -0600

    starting to set some things up

---
+++ rust/src/main.rs
@@ -0,0 +1,42 @@
+use actix_web::{self, get, middleware::{Compress, Logger}, web, App, HttpResponse, HttpServer, Responder};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct HomeTemplate {}
+
+struct AppData {
+
+}
+
+fn build_app_data() -> AppData {
+    AppData {}
+}
+
+#[get("/")]
+async fn home(app_data: web::Data<AppData>) -> impl Responder {
+    let home_template = HomeTemplate {};
+    HttpResponse::Ok().body(home_template.render().unwrap())
+}
+
+#[actix_web::main]
+async fn main() {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .format_timestamp_millis()
+        .init();
+
+    let app_data = actix_web::web::Data::new(build_app_data());
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Compress::default())
+            .app_data(app_data.clone())
+            .wrap(Logger::default())
+            .service(home)
+    })
+    .bind("0.0.0.0:8081")
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
+}
---
+++ sql/create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id serial primary key,
+    username varchar(100) NOT NULL,
+    password_hash varchar(100) NOT NULL,
+    discord_username varchar(100),
+    permission varchar(100) NOT NULL
+);
+
+CREATE TABLE videos (
+    id serial primary key,
+    video_hash varchar(100) NOT NULL,
+    frame_count integer,
+    created_ts timestamp,
+    created_user_id integer,
+    updated_ts timestamp,
+    updated_user_id integer,
+    room_id integer,
+    from_node_id integer,
+    to_node_id integer,
+    strat_id integer,
+    notes varchar(10000),
+    highlight_x_min integer,
+    highlight_x_max integer,
+    highlight_y_min integer,
+    highlight_y_max integer,
+    highlight_t_min integer,
+    highlight_t_max integer,
+    thumbnail_t integer,
+    status varchar(100) NOT NULL,
+    permanent boolean
+);
